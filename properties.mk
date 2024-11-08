c882a07ca09329a064b93b2596ba30bc9a0d35dd
Vhmit <viktor.9630@protonmail.com>
2024-11-08 10:15:32 -0300

    jeter: Set up dalvik according for init
    Change-Id: Ic78b6012edf5a67f2cda75ef1a03e5dacf63c5d3
    Signed-off-by: Vhmit <viktor.9630@protonmail.com>

--- properties.mk
+++ properties.mk
@@ -69,15 +69,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     persist.vendor.cne.feature=1
 
-# Dalvik
-PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapstartsize=16m \
-    dalvik.vm.heapgrowthlimit=256m \
-    dalvik.vm.heapsize=512m \
-    dalvik.vm.heaptargetutilization=0.75 \
-    dalvik.vm.heapminfree=4m \
-    dalvik.vm.heapmaxfree=8m
-
 # Debug
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.dbg.volte_avail_ovr=1 \
