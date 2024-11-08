f86542b3bfd0936887ab88e2c5973910b2491d8a
Bruno Martins <bgcngm@gmail.com>
2024-11-08 10:04:55 -0300

    jeter: Remove duplicate handheld_core_hardware.xml copy rule
    Since Android 10 that this is already done while inheriting from
    full_base_telephony.mk.
    
    Change-Id: I3f1e86f9e3b6f90331c43edaa2b932a816536ce7

--- device.mk
+++ device.mk
@@ -271,8 +271,7 @@ PRODUCT_PACKAGES += \
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml \
-    frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
-    frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
+    frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml
 
 # Power
 PRODUCT_PACKAGES += \
